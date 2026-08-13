70954d197c040a7cd011a249431dd3349b2d9756
Hansika <hansikareddy280807@gmail.com>
2026-08-13 11:51:41 +0530

    deployment

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # You can change this to "http://127.0.0.1:5500"
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Feedback Model
+class Feedback(BaseModel):
+    student_name: str
+    topic: str
+    rating: int
+    comment: str
+
+# GET request
+@app.get("/feedback")
+def get_feedback():
+    return {
+        "message": "Use POST request to submit feedback."
+    }
+
+# POST request
+@app.post("/feedback")
+def submit_feedback(feedback: Feedback):
+    return {
+        "message": "Feedback Submitted Successfully!",
+        "data": feedback
+    }
